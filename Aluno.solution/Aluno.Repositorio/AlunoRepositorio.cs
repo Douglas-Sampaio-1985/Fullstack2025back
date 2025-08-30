2a47c59423a3b58e2a5c575b72228eef033410d1
Douglas-Sampaio-1985 <doug.facul.enge@gmail.com>
2025-08-29 21:20:33 -0300

    Terceiro commit

---
+++ Aluno.solution/Aluno.Repositorio/AlunoRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno.Repositorio
+{
+    public class AlunoRepositorio : IAlunoRepositorio
+    {
+        public void adicionar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> listar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Aluno.solution/Aluno.Repositorio/IAlunoRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using Aluno.Dominio;
+
+namespace Aluno.Repositorio
+{
+    public interface IAlunoRepositorio
+    {
+        void adicionar();
+        List<int> listar();
+    }
+}
